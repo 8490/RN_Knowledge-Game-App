67b7a630af88dfbd0002e7e88376b5b198965309
Mert <mert.san3407@gmail.com>
2020-11-09 23:15:17 +0300

    created knowledge game App

---
+++ src/pages/Finish.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView, View, FlatList, Text, Button, TouchableOpacity} from 'react-native';
+
+import {finishPage} from './styles';
+
+import {useSelector, useDispatch} from 'react-redux'
+
+const Finish = (props) => {
+  const userScore = useSelector(global => global.score);
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={finishPage.container}>
+        <Text style={finishPage.text}>Your score is {userScore}</Text>     
+
+      </View>
+
+    </SafeAreaView>
+  );
+};
+
+export {Finish};
---
+++ src/pages/styles.js
@@ -0,0 +1,56 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+export const introPage = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3949ab'
+  },
+  bannerText: {
+    fontSize: 100,
+    color: 'white',
+    fontFamily: 'Pacifico-Regular'
+  },
+  buttonContainer: {
+    backgroundColor: '#fb8c00',
+    borderRadius: 10,
+    padding: 10,
+    paddingHorizontal: 20,
+    
+  },
+  buttonText: {
+    fontSize: 40,
+    color: 'white',
+    fontFamily: 'Pacifico-Regular',
+   
+  }
+});
+
+export const finishPage = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3949ab',
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  text: {
+    color: 'white',
+    fontSize: 100,
+    textAlign: 'center',
+    fontFamily: 'IndieFlower-Regular'
+  },
+  buttonContainer: {
+    backgroundColor: '#fb8c00',
+    borderRadius: 10,
+    padding: 10,
+    paddingHorizontal: 20,
+    width: Dimensions.get('window').width * 0.6
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'white',
+    fontFamily: 'Pacifico-Regular',
+    textAlign: "center"
+  }
+});
